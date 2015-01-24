1e93c2a08c33f436a0bc3f9f40715044120d04db
ri <git@tnu.jp>
2015-01-24 21:06:31 +0900

    not working

---
+++ PrismApp3/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Practices.Prism.StoreApps;
+using Microsoft.Practices.Unity;
+using System;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Activation;
+
+namespace PrismApp3
+{
+    sealed partial class App : MvvmAppBase
+    {
+        private readonly IUnityContainer _container = new UnityContainer();
+
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override Task OnLaunchApplication(LaunchActivatedEventArgs args)
+        {
+            NavigationService.Navigate("Main", null);
+            return Task.FromResult<object>(null);
+        }
+
+        protected override void OnInitialize(IActivatedEventArgs args)
+        {
+            _container.RegisterInstance(NavigationService);
+        }
+
+        protected override object Resolve(Type type)
+        {
+            return _container.Resolve(type);
+        }
+    }
+}
---
+++ PrismApp3/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.ObjectModel;
+using Windows.Foundation;
+using Microsoft.Practices.Prism.StoreApps;
+using Microsoft.Practices.Prism.StoreApps.Interfaces;
+
+namespace PrismApp3.ViewModels
+{
+    public class MainPageViewModel : ViewModel
+    {
+        private INavigationService _navigationService;
+
+        private ObservableCollection<Point> _points;
+
+        public ObservableCollection<Point> Points
+        {
+            get { return _points; }
+            private set { SetProperty(ref _points, value); }
+        }
+
+
+        public MainPageViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+            Points = new ObservableCollection<Point> {
+                new Point(100, 10),
+                new Point(50, 100),
+                new Point(350, 220),
+                new Point(120, 500)
+            };
+        }
+    }
+}
